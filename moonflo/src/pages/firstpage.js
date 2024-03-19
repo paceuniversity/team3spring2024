1ad4155cd2c13736f420b64f36ce847ca1c5e964
audreylouie <al83037n@pace.edu>
2024-03-19 14:49:41 -0400

    added moon animation

--- moonflo/src/pages/firstpage.js
+++ moonflo/src/pages/firstpage.js
@@ -5,7 +5,8 @@ import "../FirstPage.css";
 const firstpage = () => {
   return (
     <div className="intro">
-
+      <h1>Welcome To <br /> MoonFlo</h1>
+      <div className="moon"></div>
     </div>
   );
 };
