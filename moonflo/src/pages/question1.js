82a9705cfcffce413e1568bb41339c1d66ec5907
audreylouie <al83037n@pace.edu>
2024-03-20 22:13:57 -0400

    modified question pages to pass user's data

--- moonflo/src/pages/question1.js
+++ moonflo/src/pages/question1.js
@@ -3,25 +3,29 @@ import "../App.css";
 import "../questions.css";
 import { Button, Card, CardBody, Form, FormLabel } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useNavigate } from 'react-router-dom';
 
 const Question1 = () => {
-  const [age, setAge] = useState("");
+  const navigate = useNavigate(); // used to pass data from one page to another
+  const [age, setAge] = useState(""); // save the users age in a state
 
   const handleChange = (e) => {
     setAge(e.target.value);
   };
 
+  // when user clicks next
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Submitted age:", age);
-    // Redirect to question 2
-    window.location.href = "/question2";
+    // Redirect to question 2 while passing age data
+    navigate(`/question2?age=${age}`);
+    
   };
 
+  // when user clicks skip
   const handleSkip = () => {
     console.log("User chose to skip");
-    // Redirect to question 2
-    window.location.href = "/question2";
+    // Redirect to question 2 while passing age data
+    navigate(`/question2?age=${age}`);
   };
 
   return (
@@ -36,14 +40,14 @@ const Question1 = () => {
                 <Form.Group>
                   <FormLabel className="question">How old are you?</FormLabel>
                   <Form.Control className="custom-input"
-                    type="number"
+                    type="number" // user must enter number
                     id="age"
                     name="age"
                     placeholder="Enter your age"
-                    min="0"
+                    min="0" // value must be larger than 0
                     value={age}
                     onChange={handleChange}
-                    required
+                    required // required to fill out before clicking next
                   />
                 </Form.Group>
                 <Button variant="primary" className="next-button" type="submit">Next</Button>
