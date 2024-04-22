d5566a69cf6792ce9c13c53da9254f000a3431e2
Jennifer Garcia <98114855+Jengar21@users.noreply.github.com>
2024-04-22 12:17:50 -0400

    Revert "med info page in the works"

--- moonflo/src/components/NavBar.js
+++ moonflo/src/components/NavBar.js
@@ -40,13 +40,6 @@ const NavBar = () =>{
             </ul>
             <ul className="nav-link">
                 <li>
-                    <NavLink to="/medInfo" activeClassName="active">
-                        <BsBook size={iconSize} strokeWidth={strokeWidth}/>
-                    </NavLink>
-                </li>
-            </ul>
-            <ul className="nav-link">
-                <li>
                     <NavLink to="/settings" activeClassName="active">
                         <BsGear size={iconSize} strokeWidth={strokeWidth}/>
                     </NavLink>
